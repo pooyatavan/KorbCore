77491d609ebf3aa9d16d6f6f38b94ddb8daa81e8
Admiral <91305229+pooyatavan@users.noreply.github.com>
2025-11-18 23:46:22 +0330

    Add files via upload

--- webcore.py
+++ webcore.py
@@ -1,5 +1,5 @@
-import logging, sys, eventlet
-from eventlet import wsgi
+import logging, sys
+from gevent.pywsgi import WSGIServer
 
 # no cache files for python
 sys.dont_write_bytecode = True
@@ -7,25 +7,19 @@ sys.dont_write_bytecode = True
 from modules.log import LOG
 from modules.Captcha import Captcha
 from modules.ConfigReader import Config
+from modules.tools import network
 
 LOG.logo()
 Captcha.RemoveAllCaptchas()
+LOG.clearlogfile()
 
-if Config.read()['log']['clear'] == "enable":
-    LOG.clearlogfile()
-
-from modules.FlaskApp import app, FlaskpApp, FlaskSetup
-
+from modules.FlaskApp import app, FlaskpApp
 from modules.strings import Console
 from modules.tools import network
 
 try:
-    if Config.read()['core']['setup'] == "enable":
-        FlaskSetup()
-        LOG.info(Console.FlaskRunning.value.format(ip=network(), port="80"))
-    else:
-        FlaskpApp()
-        LOG.info(Console.FlaskRunning.value.format(ip=Config.read()['core']['ip'], port=Config.read()['core']['port']))
+    FlaskpApp()
+    LOG.info(Console.FlaskRunning.value.format(ip=network(), port=Config.read()['core']['port']))
 except:
     LOG.error(Console.FlaskError.value)
 else:
@@ -35,7 +29,5 @@ if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     logging.getLogger("waitress.queue").setLevel(logging.ERROR)
-    if Config.read()['core']['setup'] == "disable":
-        wsgi.server(eventlet.listen((str(Config.read()['core']['ip']), int(Config.read()['core']['port']))), app, log_output=False)
-    else:
-        wsgi.server(eventlet.listen((str(network()), 80)), app, log_output=False)
+    http_server = WSGIServer((network(), int(Config.read()['core']['port'])), app, log=None)
+    http_server.serve_forever()
